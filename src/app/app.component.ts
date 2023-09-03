fa0c952fa111162892f5cdef256cf00d00db090d
paulohcatto@gmail.com <paulohcatto@gmail.com>
2023-09-03 15:54:10 -0300

    add projeto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,68 @@
-import { Component } from '@angular/core';
+import { MusicService } from './services/music.service';
+import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { Music } from './models/model.music';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'projeto-api';
+export class AppComponent implements OnInit {
+  musicas$ = new Observable<Music[]>();
+  public id = '';
+  public musica = '';
+  public author = '';
+
+  constructor(private musicService: MusicService) {
+    this.obterMusicasCadastradas();
+  }
+
+  ngOnInit(): void {}
+
+  obterMusicasCadastradas() {
+    this.musicas$ = this.musicService.obterMusicas();
+  }
+
+  buttonClick() {
+    if (!this.musica || !this.author) {
+      alert('Preencha os campos');
+      return;
+    }
+
+    if (this.id) {
+      this.atualizarMusica();
+      return;
+    }
+
+    this.musicService
+      .cadastrarMusicas({
+        author: this.author,
+        text: this.musica,
+      })
+      .subscribe(() => this.obterMusicasCadastradas());
+  }
+
+  atualizarMusica() {
+    this.musicService
+      .editarMusica({
+        id: parseInt(this.id),
+        author: this.author,
+        text: this.musica,
+      })
+      .subscribe(() => this.obterMusicasCadastradas());
+  }
+
+  preencherCampos(musica: Music) {
+    this.id = musica.id!.toString();
+    this.musica = musica.text;
+    this.author = musica.author;
+  }
+
+  removerMusica(id: number) {
+    this.musicService
+      .remover(id)
+      .subscribe((_) => this.obterMusicasCadastradas());
+  }
 }
---
+++ src/app/services/music.service.ts
@@ -0,0 +1,31 @@
+import { environment } from './../../environments/environment';
+import { Injectable, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Music, MusicCadastrar } from '../models/model.music';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class MusicService {
+
+  private url = `${environment.api}/musics`;
+
+  constructor(private httpClient: HttpClient) {}
+
+  obterMusicas(): Observable<Music[]> {
+    return this.httpClient.get<Music[]>(this.url);
+  }
+
+  cadastrarMusicas(musica: MusicCadastrar) {
+    return this.httpClient.post<Music>(this.url, musica)
+  }
+
+  editarMusica(musica: Music) {
+    return this.httpClient.put<Music>(`${this.url}/${musica.id}`, musica)
+  }
+
+  remover(id: number) {
+    return this.httpClient.delete<void>(`${this.url}/${id}`)
+  }
+}
